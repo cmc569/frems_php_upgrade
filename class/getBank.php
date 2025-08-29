6ec3f4a4a93b2dc976bdddc4461a9ed436d9e79c
Jason Chen <cmc569@gmail.com>
2025-08-29 11:59:35 +0800

    修正銀行資訊取得資料庫連線物件

--- class/getBank.php
+++ class/getBank.php
@@ -1,5 +1,6 @@
 <?php
 /* 銀行相關資料取得 */
+require_once dirname(__DIR__) . '/first1DB.php';
 
 //取得銀行總行資料
 function getBankMain($_conn, $main = '')
@@ -10,16 +11,17 @@ function getBankMain($_conn, $main = '')
     }
     $str .= '>總行' . "</option>\n";
 
+    $_conn = new first1DB();
+
     $sql = 'SELECT * FROM tBank WHERE bBank4="" ORDER BY bBank3 ASC;';
-    $rs  = $_conn->Execute($sql);
-    if ($rs && ! $rs->EOF) {
-        while (! $rs->EOF) {
-            $str .= '<option value="' . $rs->fields['bBank3'] . '"';
-            if ($rs->fields['bBank3'] == $main) {
+    $rs  = $_conn->all($sql);
+    if (! empty($rs)) {
+        foreach ($rs as $row) {
+            $str .= '<option value="' . $row['bBank3'] . '"';
+            if ($row['bBank3'] == $main) {
                 $str .= ' selected="selected"';
             }
-            $str .= '>' . $rs->fields['bBank3_name'] . '(' . $rs->fields['bBank3'] . ')' . "</option>\n";
-            $rs->MoveNext();
+            $str .= '>' . $row['bBank3_name'] . '(' . $row['bBank3'] . ')' . "</option>\n";
         }
     }
 
@@ -36,16 +38,16 @@ function getBankBranch($_conn, $main = '', $branch = '')
     }
     $str .= '>分行' . "</option>\n";
 
-    $sql = 'SELECT * FROM tBank WHERE bBank3="' . $main . '" AND bBank4<>"" AND bOK =0 ORDER BY bBank4 ASC;';
-    $rs  = $_conn->Execute($sql);
-    if ($rs && ! $rs->EOF) {
-        while (! $rs->EOF) {
-            $str .= '<option value="' . $rs->fields['bBank4'] . '"';
-            if ($rs->fields['bBank4'] == $branch) {
+    $_conn = new first1DB();
+    $sql   = 'SELECT * FROM tBank WHERE bBank3="' . $main . '" AND bBank4<>"" AND bOK =0 ORDER BY bBank4 ASC;';
+    $rs    = $_conn->all($sql);
+    if (! empty($rs)) {
+        foreach ($rs as $row) {
+            $str .= '<option value="' . $row['bBank4'] . '"';
+            if ($row['bBank4'] == $branch) {
                 $str .= ' selected="selected"';
             }
-            $str .= '>' . $rs->fields['bBank4_name'] . '(' . $rs->fields['bBank4'] . ')' . "</option>\n";
-            $rs->MoveNext();
+            $str .= '>' . $row['bBank4_name'] . '(' . $row['bBank4'] . ')' . "</option>\n";
         }
     }
 
@@ -60,12 +62,13 @@ function getBankBranchName($_conn, $main = '', $branch = '')
         return '';
     }
 
+    $_conn = new first1DB();
+
     $sql = 'SELECT * FROM tBank WHERE bBank3="' . $main . '" AND bBank4<>"" AND bBank4="' . $branch . '" ORDER BY bBank4 ASC;';
+    $rs  = $_conn->one($sql);
 
-    $rs = $_conn->Execute($sql);
-
-    if ($rs && ! $rs->EOF && isset($rs->fields['bBank4_name']) && isset($rs->fields['bBank4'])) {
-        $str = $rs->fields['bBank4_name'] . '(' . $rs->fields['bBank4'] . ')';
+    if (! empty($rs) && isset($rs['bBank4_name']) && isset($rs['bBank4'])) {
+        $str = $rs['bBank4_name'] . '(' . $rs['bBank4'] . ')';
         return $str;
     }
 
